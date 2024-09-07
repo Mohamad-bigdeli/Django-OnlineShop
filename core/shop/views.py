32005f7013da0902cb8064565f80c1d253eccf66
Mohamad-bigdeli <mohamadbigdeli24@gmail.com>
2024-09-08 01:09:25 +0330

    -

--- core/shop/views.py
+++ core/shop/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
 # Create your views here.
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category)
+    context = {
+        'products' : products,
+        'category' : category,
+        'categories' : categories
+    }
+    return render(request, 'shop/product_list.html', context)
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, slug=slug, id=id)
+    context = {
+        'product' : product 
+    }
+    return render(request, 'shop/product_detail.html', context)
