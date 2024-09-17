f196554584841554e81329d6986b865860361fc2
Mohamad-bigdeli <mohamadbigdeli24@gmail.com>
2024-09-17 20:54:53 +0330

    -

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -7,7 +7,38 @@ class ShopUserCreationForm(UserCreationForm):
         model = ShopUser
         fields = ['phone', 'first_name', 'last_name', 'address', 'is_active', 'is_staff', 'is_superuser', 'date_joined']
 
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if self.isinstance.pk:
+            if ShopUser.objects.filter(phone=phone).exclude(pk=self.instance.pk).exists():
+                raise forms.ValidationError('Phone already exist.')
+        else:
+            if ShopUser.objects.filter(phone=phone).exists(): 
+                raise forms.ValidationError('Phone already exist.')
+        if not phone.isdigit():
+            raise forms.ValidationError('Invalid phone number.')
+        if not phone.startwith('09'):
+            raise forms.ValidationError('Phone must start with 09 digits.')
+        if len(phone) != 11:
+            raise forms.ValidationError('Phone must have 11 digits.')
+        return phone
+
 class ShopUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = ShopUser
         fields = ['phone', 'first_name', 'last_name', 'address', 'is_active', 'is_staff', 'is_superuser', 'date_joined']
+    
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if self.isinstance.pk:
+            if ShopUser.objects.filter(phone=phone).exclude(pk=self.instance.pk).exists():
+                raise forms.ValidationError('Phone already exist.')
+        else:
+            if ShopUser.objects.filter(phone=phone).exists(): 
+                raise forms.ValidationError('Phone already exist.')
+        if not phone.isdigit():
+            raise forms.ValidationError('Invalid phone number.')
+        if not phone.startwith('09'):
+            raise forms.ValidationError('Phone must start with 09 digits.')
+        if len(phone) != 11:
+            raise forms.Validat
